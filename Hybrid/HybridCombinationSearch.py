9197efd8439d4bc095d0a33daae4670b2a1a4603
Filippo Colombo <filippo7.colombo@mail.polmi.it>
2020-12-03 13:12:45 +0100

    plot with merged combo

--- Hybrid/HybridCombinationSearch.py
+++ Hybrid/HybridCombinationSearch.py
@@ -1,5 +1,6 @@
 from Base.BaseSimilarityMatrixRecommender import BaseItemSimilarityMatrixRecommender
 from Base.DataIO import DataIO
+from Base.Recommender_utils import check_matrix, similarityMatrixTopK
 
 from KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
 from KNN.UserKNNCFRecommender import UserKNNCFRecommender
@@ -57,4 +58,56 @@ class HybridCombinationSearch(BaseItemSimilarityMatrixRecommender):
         dataIO = DataIO(folder_path=folder_path)
         dataIO.save_data(file_name=file_name, data_dict_to_save = {})
         self._print("Saving complete")
+
+
+class HybridCombinationMergedSearch(BaseItemSimilarityMatrixRecommender):
+    RECOMMENDER_NAME = "HybridCombinationMergedSearch"
+
+    # set the seed equal to the one of the parameter search!!!!
+    def __init__(self, URM_train, ICM_train, list_rec=None, verbose=True, seed=1205):
+        super(HybridCombinationMergedSearch, self).__init__(URM_train, verbose=verbose)
+        self.URM_train = URM_train
+        self.ICM_train = ICM_train
+
+        self.__rec1 = list_rec[0]
+        self.__rec2 = list_rec[1]
+        self.__rec3 = list_rec[2]
+
+        if (self.__rec1.W_sparse.shape != self.__rec2.W_sparse.shape) or \
+                (self.__rec1.W_sparse.shape != self.__rec3.W_sparse.shape):
+            raise ValueError(
+                f"HybridCombinationMergedSearch({self.__rec1.RECOMMENDER_NAME} - {self.__rec2.RECOMMENDER_NAME} - "
+                f"{self.__rec3.RECOMMENDER_NAME}): similarities have different size, S1 is "
+                f"{self.__rec1.W_sparse.shape}, S2 is {self.__rec2.W_sparse.shape}, S3 is {self.__rec3.W_sparse.shape}")
+
+        self.__W1 = check_matrix(self.__rec1.W_sparse.copy(), 'csr')
+        self.__W2 = check_matrix(self.__rec2.W_sparse.copy(), 'csr')
+        self.__W3 = check_matrix(self.__rec3.W_sparse.copy(), 'csr')
+
+        self.__a = self.__b = self.__c = None
+        self.topK=None
+        self.W_sparse=None
+        self.seed = seed
+
+    def fit(self, alpha=0.5, l1_ratio=0.5, topK=100):
+        self.__a = alpha * l1_ratio
+        self.__b = alpha - self.__a
+        self.__c = 1 - self.__a - self.__b
+        self.topK = topK
+
+        W = self.__W1 * self.__a \
+            + self.__W2 * self.__b \
+            + self.__W3 * self.__c
+
+        self.W_sparse = similarityMatrixTopK(W, k=self.topK).tocsr()
+
+    def save_model(self, folder_path, file_name=None):
+        if file_name is None:
+            file_name = self.RECOMMENDER_NAME
+        self._print("Saving model in file '{}'".format(folder_path + file_name))
+        dataIO = DataIO(folder_path=folder_path)
+        dataIO.save_data(file_name=file_name, data_dict_to_save={})
+        self._print("Saving complete")
+
+
 
