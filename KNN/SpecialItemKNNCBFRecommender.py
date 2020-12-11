f38f27f89e9f2b4ddd61964ee223f088cd5a320e
Filippo Colombo <filippo7.colombo@mail.polmi.it>
2020-12-11 12:20:35 +0100

    up

--- KNN/SpecialItemKNNCBFRecommender.py
+++ KNN/SpecialItemKNNCBFRecommender.py
@@ -5,8 +5,8 @@ from KNN.ItemKNNCBFRecommender import ItemKNNCBFRecommender
 class SpecialItemKNNCBFRecommender(ItemKNNCBFRecommender):
     RECOMMENDER_NAME = "Special-ItemKNNCBFRec"
 
-    def __init__(self, URM_train, ICM_train):
+    def __init__(self, URM_train, ICM_train, verbose = True):
         parser = DataParser()
         icm = parser.get_Special_ICM_all()
-        super(SpecialItemKNNCBFRecommender, self).__init__(URM_train, icm)
+        super(SpecialItemKNNCBFRecommender, self).__init__(URM_train, icm, verbose)
         self.ICM_train=icm
