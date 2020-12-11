489251e781e6e20621921ed958b2fffd033e9ac3
Giovanni <giovanni.delvecchio@mail.polimi.it>
2020-12-11 11:51:46 +0100

    search

--- run_parameter_search_hybrid_CV.py
+++ run_parameter_search_hybrid_CV.py
@@ -18,6 +18,7 @@ from Hybrid.PipeHybrid001 import PipeHybrid001
 from KNN.ItemKNNCBFRecommender import ItemKNNCBFRecommender
 from KNN.UserKNNCFRecommender import UserKNNCFRecommender
 from KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
+from KNN.ItemKNN_CBF_CF import ItemKNN_CBF_CF
 from MatrixFactorization.IALSRecommender import IALSRecommender
 from SLIM_BPR.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
 from SLIM_ElasticNet.SLIMElasticNetRecommender import SLIMElasticNetRecommender
@@ -59,7 +60,7 @@ def read_data_split_and_search():
     URM_all = parser.get_URM_all()
     ICM_obj = parser.get_ICM_all()
 
-    URM_train, URM_test = split_train_in_two_percentage_global_sample(URM_all, train_percentage=0.85, seed=seed)
+    #URM_train, URM_test = split_train_in_two_percentage_global_sample(URM_all, train_percentage=0.85, seed=seed)
     #URM_train, URM_validation = split_train_in_two_percentage_global_sample(URM_train, train_percentage=0.85, seed=seed)
     k = 5
 
@@ -72,11 +73,14 @@ def read_data_split_and_search():
     from Base.Evaluation.Evaluator import EvaluatorHoldout
 
     combo_algorithm_list = [
-        (ItemKNNCBFRecommender, {'topK': 22, 'shrink': 59, 'similarity': 'dice', 'normalize': False}),
-        (ItemKNNCFRecommender, {'topK': 994, 'shrink': 981, 'similarity': 'asymmetric', 'normalize': True, 'asymmetric_alpha': 0.05110465631417439, 'feature_weighting': 'TF-IDF'}),
-        (UserKNNCFRecommender, {'topK': 342, 'shrink': 1, 'similarity': 'cosine', 'normalize': True, 'feature_weighting': 'none'}),
-        (RP3betaRecommender, {'topK': 121, 'alpha': 0.20195468746532336, 'beta': 0.004377103282444673, 'normalize_similarity': False}),
-        (P3alphaRecommender, {'topK': 344, 'alpha': 0.4233795425391197, 'normalize_similarity': False})
+        (ItemKNNCBFRecommender), {'topK': 164, 'shrink': 8, 'similarity': 'jaccard', 'normalize': True}
+        (ItemKNNCBF_Special), {'topK': 1000, 'shrink': 1000, 'similarity': 'cosine', 'normalize': True, 'feature_weighting': 'BM25'},
+        (ItemKNN_CBF_CF), {'topK': 1000, 'shrink': 1000, 'similarity': 'asymmetric', 'normalize': True, 'asymmetric_alpha': 0.241892724784089, 'feature_weighting': 'TF-IDF', 'icm_weight': 1.0},
+        (ItemKNNCFRecommender), {'topK': 1000, 'shrink': 1000, 'similarity': 'cosine', 'normalize': True, 'feature_weighting': 'TF-IDF'},
+        (UserKNNCFRecommender), {'topK': 163, 'shrink': 846, 'similarity': 'cosine', 'normalize': True, 'feature_weighting': 'TF-IDF'},
+        (RP3betaRecommender), {'topK': 926, 'alpha': 0.4300109351916609, 'beta': 0.01807360750913967, 'normalize_similarity': False},
+        (P3alphaRecommender), {'topK': 575, 'alpha': 0.48009885897470206, 'normalize_similarity': False},
+        #(SLIM_BPR_Cython, {'topK': 989, 'epochs': 90, 'symmetric': False, 'sgd_mode': 'adam', 'lambda_i': 1.7432198130463203e-05, 'lambda_j': 0.0016819750046109673, 'learning_rate': 0.00031293205801039345})
     ]
     list_already_seen = []
     combinations_already_seen = combinations(list_already_seen, 3)
@@ -84,33 +88,35 @@ def read_data_split_and_search():
     (icb, icf, p3a), (icb, icf, rp3b), (icb, icf, sen), (icb, p3a, rp3b), (icb, p3a, sen),
                                 (icb, rp3b, sen), (icf, p3a, rp3b), (icf, p3a, sen)
     """
+    combination_to_be_done = list(combinations(combo_algorithm_list, 3))
 
-    for rec_perm in combinations(combo_algorithm_list, 3):
+    for rec_perm in combination_to_be_done[]:
 
         if rec_perm not in combinations_already_seen:
             recommender_names = '_'.join([r[0].RECOMMENDER_NAME for r in rec_perm])
-            output_folder_path = "result_experiments_CV/seed_" + str(seed) + '/linear_combination/' + recommender_names + '/'
+            output_folder_path = "result_experiments_CV2/seed_" + str(seed) + '/linear/' + recommender_names + '/'
             print(F"\nTESTING THE COMBO {recommender_names}")
 
-            # If directory does not exist, create
-            if not os.path.exists(output_folder_path):
-                os.makedirs(output_folder_path)
+            if (ItemKNNCBFRecommender not in rec_perm) and (ItemKNNCBF_Special not in rec_perm) and (ItemKNN_CBF_CF not in rec_perm):
+                # If directory does not exist, create
+                if not os.path.exists(output_folder_path):
+                    os.makedirs(output_folder_path)
 
-            runParameterSearch_Collaborative_partial = partial(runParameterSearch_Collaborative,
-                                                               URM_train = URM_train,
-                                                               ICM_train = ICM_obj,
-                                                               metric_to_optimize = "MAP",
-                                                               n_cases = 50,
-                                                               n_random_starts = 20,
-                                                               output_folder_path = output_folder_path,
-                                                               parallelizeKNN = False,
-                                                               allow_weighting = False,
-                                                               k = k,
-                                                               seed = seed,
-                                                               list_rec = rec_perm,
-                                                               level = 'hybrid_search')
-            pool = multiprocessing.Pool(processes=int(multiprocessing.cpu_count()), maxtasksperchild=1)
-            pool.map(runParameterSearch_Collaborative_partial, collaborative_algorithm_list)
+                    runParameterSearch_Collaborative_partial = partial(runParameterSearch_Collaborative,
+                                                                       URM_train = URM_all,
+                                                                       ICM_train = ICM_obj,
+                                                                       metric_to_optimize = "MAP",
+                                                                       n_cases = 50,
+                                                                       n_random_starts = 20,
+                                                                       output_folder_path = output_folder_path,
+                                                                       parallelizeKNN = False,
+                                                                       allow_weighting = False,
+                                                                       k = k,
+                                                                       seed = seed,
+                                                                       list_rec = rec_perm,
+                                                                       level = 'hybrid_search')
+                    pool = multiprocessing.Pool(processes=int(multiprocessing.cpu_count()), maxtasksperchild=1)
+                    pool.map(runParameterSearch_Collaborative_partial, collaborative_algorithm_list)
 
 
 if __name__ == '__main__':
